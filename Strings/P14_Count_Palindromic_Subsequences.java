a256146ebb59193311fdc6eb183bccd8beea2c46
Raiyaan Khan <kraiyaan@gmail.com>
2022-11-04 09:39:22 +0530

    fix required

--- Strings/P14_Count_Palindromic_Subsequences.java
+++ Strings/P14_Count_Palindromic_Subsequences.java
@@ -1,40 +1,28 @@
-package Strings;
+// package Strings;
 
-public class P14_Count_Palindromic_Subsequences {
+// public class P14_Count_Palindromic_Subsequences {
 
-    public static boolean isPalindrome(String s) {
-        if(s.length()==1){
-            return true;
-        }
-        String s1 = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            s1 = s1 + s.charAt(i);
-        }
-        if (s1.equalsIgnoreCase(s)) {
-            return true;
-        } else
-            return false;
-    }
+//     public static int check(int i, int j, String s, int[][] arr) {
 
-    static long countPS(String str) {
-         long count =0;
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i; j < str.length(); j++) {
-                if(isPalindrome(str.substring(i,j))){
-                    count++;
-                    System.out.println(str.substring(i,j));
-                    System.out.println(isPalindrome(str.substring(i,j)));
-                }
-            }
-        }
-        return count;
-    }
+        
+//         if(i>j) return 0;
+//         if(i==j) return 1;
 
-    public static void main(String[] args) {
-        System.out.println(countPS("aab"));
+//         if(arr[i][j])
+//     }
+
+//     static long countPS(String str) {
+         
+//         int[][] arr = new int[str.length()][str.length()];
+
+
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(countPS("aab"));
         
         
         
-    }
+//     }
 
-}
+// }
