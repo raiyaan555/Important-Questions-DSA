3a4bb96847bf6075dfab76e516a447ef352fb355
Raiyaan Khan <kraiyaan@gmail.com>
2023-01-20 16:06:33 +0530

    question 2 solved

---
+++ Searching_and_Sorting/Minimum_Moves.java
@@ -0,0 +1,37 @@
+package Searching_and_Sorting;
+
+import java.util.*;
+
+public class Minimum_Moves {
+    public static int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+        // int sum;
+        int ans = 0;
+
+        // if(nums.length%2==0){
+
+        // sum = (nums[nums.length/2]+nums[(nums.length/2) +1])/2;
+
+        // }else{
+        // sum = nums[nums.length/2];
+        // }
+
+        while (start < end) {
+
+            ans += nums[end] - nums[start];
+
+            end--;
+            start++;
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 7, 8, 11 };
+
+        System.out.println(minMoves2(nums));
+    }
+}
