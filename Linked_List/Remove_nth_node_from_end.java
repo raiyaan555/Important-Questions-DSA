632b4f07c23f279a3227c867ea5cce34ace94aaa
Raiyaan Khan <kraiyaan@gmail.com>
2023-01-24 06:49:38 +0530

    commit

---
+++ Linked_List/Remove_nth_node_from_end.java
@@ -0,0 +1,36 @@
+package Linked_List;
+
+public class Remove_nth_node_from_end {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
