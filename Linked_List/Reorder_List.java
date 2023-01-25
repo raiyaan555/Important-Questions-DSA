466ff2181bce44432c511bf187de2cee6708b1ec
Raiyaan Khan <kraiyaan@gmail.com>
2023-01-25 06:12:55 +0530

    commit

---
+++ Linked_List/Reorder_List.java
@@ -0,0 +1,58 @@
+package Linked_List;
+
+public class Reorder_List {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = reverse(slow.next);
+        slow.next = null;
+        ListNode first = head;
+
+        while (second != null) {
+            ListNode temp = first.next;
+            ListNode temp1 = second.next;
+            first.next = second;
+            second.next = temp;
+            first = temp;
+            second = temp1;
+        }
+
+    }
+}
