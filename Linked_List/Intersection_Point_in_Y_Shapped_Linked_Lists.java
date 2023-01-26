cc6f31d8d07b7cf2b4d5dcf95437b3108d0cbbfc
Raiyaan Khan <kraiyaan@gmail.com>
2023-01-26 11:58:45 +0530

    commit

---
+++ Linked_List/Intersection_Point_in_Y_Shapped_Linked_Lists.java
@@ -0,0 +1,39 @@
+package Linked_List;
+
+public class Intersection_Point_in_Y_Shapped_Linked_Lists {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    Node head; // head of list
+
+    int intersectPoint(Node head1, Node head2) {
+        Node a = head1;
+        Node b = head2;
+
+        while (a != b) {
+            if (a == null)
+
+                a = head2;
+            else {
+                a = a.next;
+
+            }
+            if (b == null) {
+                b = head1;
+
+            } else {
+                b = b.next;
+
+            }
+
+        }
+        return a.data;
+    }
+}
