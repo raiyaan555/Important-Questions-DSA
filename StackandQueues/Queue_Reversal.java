b3ed7b92bf539a7cb1f37ae98908542b5e6910cf
Raiyaan Khan <kraiyaan@gmail.com>
2023-01-31 08:45:23 +0530

    commit

---
+++ StackandQueues/Queue_Reversal.java
@@ -0,0 +1,22 @@
+package StackandQueues;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class Queue_Reversal {
+    public Queue<Integer> rev(Queue<Integer> q) {
+
+        Stack<Integer> s = new Stack<Integer>();
+
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+
+        return q;
+
+    }
+}
