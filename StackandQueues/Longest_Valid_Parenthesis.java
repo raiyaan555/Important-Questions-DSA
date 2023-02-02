f61a94b86509e0b7e567e80cd5c6c935cb6649e5
Raiyaan Khan <kraiyaan@gmail.com>
2023-02-02 16:54:18 +0530

    commit

--- StackandQueues/Longest_Valid_Parenthesis.java
+++ StackandQueues/Longest_Valid_Parenthesis.java
@@ -1,5 +1,27 @@
 package StackandQueues;
 
+import java.util.Stack;
+
 public class Longest_Valid_Parenthesis {
-    
+    static int maxLength(String S){
+        // code here
+        Stack<Integer> st = new Stack<>();
+        st.add(-1);
+        int count = Integer.MIN_VALUE;
+       for(int i = 0; i<S.length();i++){
+        char c = S.charAt(i);
+            if(c=='('){
+               st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    count = Math.max(count, i-st.peek());
+                }
+
+            }
+        }
+        return count;
+    }
 }
