bf216b41a0274b0c539a06cc2a491e60d82013da
Raiyaan Khan <kraiyaan@gmail.com>
2023-02-06 17:24:51 +0530

    second question commit

---
+++ Greedy/Minimum_number_of_Coins.java
@@ -0,0 +1,24 @@
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Minimum_number_of_Coins {
+    static List<Integer> minPartition(int N) {
+        List<Integer> list = new ArrayList<>();
+        // code here
+        int[] currency = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
+
+        for (int i = currency.length - 1; i >= 0; i--) {
+
+            while (N >= currency[i]) {
+                list.add(currency[i]);
+                N -= currency[i];
+            }
+
+        }
+
+        return list;
+
+    }
+}
