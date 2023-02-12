e397b09e9a3c646850363833c4cba8a2b2ea4efb
Raiyaan Khan <kraiyaan@gmail.com>
2023-02-12 13:15:47 +0530

    commit

--- Arrays/P23_SubArraySum_Divisible_by_k_leetcode.java
+++ Arrays/P23_SubArraySum_Divisible_by_k_leetcode.java
@@ -5,7 +5,7 @@ public class P23_SubArraySum_Divisible_by_k_leetcode {
     public static int subarraysDivByK(int[] nums, int k) {
         int total = 0, sum = 0;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+       HashMap<Integer,Integer> map = new HashMap<>();
         map.put(0, 1);
 
         for (int i = 0; i < nums.length; i++) {
