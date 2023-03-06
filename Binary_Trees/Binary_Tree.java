cd7803e61db5a45a05d0007ae2025bf85a1d0f24
Raiyaan Khan <kraiyaan@gmail.com>
2023-03-07 05:26:07 +0530

    lowest common ancestor

--- Binary_Trees/Binary_Tree.java
+++ Binary_Trees/Binary_Tree.java
@@ -250,6 +250,26 @@ public class Binary_Tree {
     }
 
 
+    // lowest common ancestor
+
+
+    Node lowestCommonAncestor(Node root, int n1, int n2){
+        if(root ==null) return null;
+        if(root.data ==n1 || root.data ==n2) return root;
+
+
+        Node left = lowestCommonAncestor(root.left, n1, n2);
+        Node right = lowestCommonAncestor(root.right, n1, n2);
+
+        if(left==null) return right;
+        if(right==null) return left;
+        return root;
+    }
+
+
+
+
+
 
 
 
