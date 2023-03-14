6ae97af02f10cd4498068a27c24ab923841d97e8
Raiyaan Khan <kraiyaan@gmail.com>
2023-03-14 06:45:11 +0530

    kth smallest

--- Heap_and_Hashing/Print_kth_largest_element_in_array.java
+++ Heap_and_Hashing/Print_kth_largest_element_in_array.java
@@ -1,9 +1,11 @@
 package Heap_and_Hashing;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Print_kth_largest_element_in_array {
-    int kthLargest(int a[], int k){
+    static int kthLargest(int a[], int k){
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i<k; i++){
@@ -17,5 +19,28 @@ public class Print_kth_largest_element_in_array {
             }
         }
         return pq.peek();
+    }
+
+    static int kthSmallest(int a[], int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i =0; i<k;i++){
+            pq.add(a[i]);
+        }
+
+        for(int i = k; i<a.length;i++){
+            if(pq.peek() > a[i]){
+                pq.remove();
+                pq.add(a[i]);
+            }
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {20, 10,60,30,40,50};
+
+        System.out.println(kthLargest(arr, 3));
+        System.out.println(kthSmallest(arr, 3));
     }
 }
