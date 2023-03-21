1f694418f59b2189adc4291bc65585010b6985c0
Raiyaan Khan <kraiyaan@gmail.com>
2023-03-21 06:38:00 +0530

    bfs

---
+++ Graphs/BFS.java
@@ -0,0 +1,34 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    
+    public ArrayList<Integer> bfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj){
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        boolean vis[] = new boolean[v];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0);
+        vis[0] = true;
+
+
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            bfs.add(node);
+
+
+            for(Integer i:adj.get(node)){
+                if(vis[i]==false){
+                    vis[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+        return bfs;
+    }
+}
