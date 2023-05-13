ab32bad95c2feb78a2420a61fa3e50a19fe1aa98
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-13 07:27:41 +0530

    set_matrix_zero_brute

--- Two_D_Arrays/Set_Matrix_Zeroes.java
+++ Two_D_Arrays/Set_Matrix_Zeroes.java
@@ -1,5 +1,8 @@
 package Two_D_Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Set_Matrix_Zeroes {
     public static void setZeroes(int[][] matrix) {
         int n = matrix.length;
@@ -125,5 +128,36 @@ class Solution {
             }
         }
 
+    }
+
+    // brute force solution
+
+    class Solution2 {
+        public void setZeroes(int[][] matrix) {
+            int R = matrix.length;
+            int C = matrix[0].length;
+            Set<Integer> rows = new HashSet<Integer>();
+            Set<Integer> cols = new HashSet<Integer>();
+
+            // Essentially, we mark the rows and columns that are to be made zero
+            for (int i = 0; i < R; i++) {
+                for (int j = 0; j < C; j++) {
+                    if (matrix[i][j] == 0) {
+                        rows.add(i);
+                        cols.add(j);
+                    }
+                }
+            }
+
+            // Iterate over the array once again and using the rows and cols sets, update
+            // the elements.
+            for (int i = 0; i < R; i++) {
+                for (int j = 0; j < C; j++) {
+                    if (rows.contains(i) || cols.contains(j)) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
     }
 }
