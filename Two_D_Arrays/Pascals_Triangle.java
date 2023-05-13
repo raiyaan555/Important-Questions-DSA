bec6ddb5d3faf9d2de1918d972a11d92951c970b
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-13 12:04:16 +0530

    pascal_trainagle.java

---
+++ Two_D_Arrays/Pascals_Triangle.java
@@ -0,0 +1,28 @@
+package Two_D_Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// optimal solution
+
+public class Pascals_Triangle {
+    public List<Integer> findAns(int r) {
+        int ans = 1;
+        List<Integer> answer = new ArrayList<>();
+        answer.add(1);
+        for (int j = 1; j < r; j++) {
+            ans = ans * (r - j);
+            ans = ans / j;
+            answer.add(ans);
+        }
+        return answer;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> finalAns = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            finalAns.add(findAns(i));
+        }
+        return finalAns;
+    }
+}
