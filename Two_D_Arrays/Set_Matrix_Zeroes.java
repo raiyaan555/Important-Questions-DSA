f8837b9fce96b43801c463861e2fcbe6bdfa8328
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-13 07:23:31 +0530

    set_matrix_zero_optimal_soln

--- Two_D_Arrays/Set_Matrix_Zeroes.java
+++ Two_D_Arrays/Set_Matrix_Zeroes.java
@@ -59,19 +59,71 @@ public class Set_Matrix_Zeroes {
 
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
-        int k=0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                arr[i][j]=k++;
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = k++;
             }
         }
         setZeroes(arr);
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
         }
+    }
+}
+
+/*
+ * optimal solution
+ * 
+ * time complexity of the down below code is 2*(n*m) almost exact will be n*m +
+ * n-1*m-1
+ * 
+ * space complexity will be 0(1);
+ * 
+ */
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
     }
 }
