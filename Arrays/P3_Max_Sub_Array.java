92e5ac9b55028263fbe82f22feabd56af7802ca6
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-14 15:03:47 +0530

    commit

--- Arrays/P3_Max_Sub_Array.java
+++ Arrays/P3_Max_Sub_Array.java
@@ -20,6 +20,25 @@ public class P3_Max_Sub_Array {
 
     }
 
+    // optimal soln 2 with time O(N) and space O(2);
+
+    public int maxSubArray2(int[] nums) {
+
+        int currSum = 0;
+        int maxSum = nums[0];
+ 
+        for (int i : nums){
+            if(currSum <0){
+                currSum = 0;
+            }
+            currSum = currSum+i;
+            maxSum = Math.max(maxSum,currSum);
+        }
+ 
+        return maxSum;
+ 
+     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array");
