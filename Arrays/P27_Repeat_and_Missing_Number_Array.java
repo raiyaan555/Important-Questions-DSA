04e7b58b15c89f120f086058d4b94cc331e78eba
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-18 13:16:38 +0530

    commit

--- Arrays/P27_Repeat_and_Missing_Number_Array.java
+++ Arrays/P27_Repeat_and_Missing_Number_Array.java
@@ -2,49 +2,34 @@ package Arrays;
 import java.util.*;
 
 class P27_Repeat_and_Missing_Number_Array{
-    public static int findDuplicate(int[] nums){
-        Arrays.sort(nums);
-        
-        for(int i = 0; i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+    // optimal solution is in the order of 
+    public class Solution {
+        // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+        public int[] repeatedNumber(final int[] A) {
+            int[]ans = new int[2];
+            long n = A.length;
+            
+            long Sn = ((n)*(n+1))/2;
+            long S2n = (n*(n+1)*(2*n+1))/6;
+            
+            long S =0, S2 = 0;
+            
+            for(int i: A){
+                S += i;
+                S2 += (long)i*(long)i;
+            } 
+            
+            long val1 = S-Sn;
+            long val2 = S2-S2n;
+            val2 = val2/val1;
+            long x = (val1+val2)/2;
+            long y = val1-x;
+            
+            ans[0] = (int)x;
+            ans[1] = (int)y;
+            return ans;
+            
         }
-        return -1;
     }
     
-    public static int findMissing(int[] A){
-        
-
-        int sum = 0, actualSum = 0;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for(int i =0; i<A.length;i++){
-            if(A[i]>=max){
-                max = A[i];
-            }
-            if(A[i]<=min){
-                min = A[i];
-            }
-        }
-        for(int i = min; i<=max;i++){
-            sum += i;
-        }
-        
-        for(int i = 0; i<A.length;i++){
-            actualSum +=A[i];
-        }
-        int a = findDuplicate(A);
-        return Math.abs(sum-(Math.abs(actualSum-a)));
-    }
-    
-    public int[] repeatedNumber(final int[] A) {
-        
-        int[] ans = new int[2];
-        
-        ans[0] = findDuplicate(A);
-        ans[1] = findMissing(A);
-        
-        return ans;
-    }
 }
