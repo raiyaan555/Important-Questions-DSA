211a90e8742f87ba7f5e777c591df911bd2ee8cc
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-19 10:04:31 +0530

    majority_element_>n/2

--- Searching_and_Sorting/Print_Majority_Voting_Moore.java
+++ Searching_and_Sorting/Print_Majority_Voting_Moore.java
@@ -4,61 +4,47 @@ import javax.swing.text.DefaultStyledDocument.ElementSpec;
 
 public class Print_Majority_Voting_Moore {
 
-    public static int majorityProbable(int a[]) {
-        int majority = 0;
-        int count = 1;
-       
-        
+    public static int majorityElement(int[] nums) {
+        int count = 0; 
+        int el =0;
 
-        for (int i = 1; i < a.length; i++) {
-            if (a[majority] == a[i]) {
+
+        for(int i =0; i<nums.length; i++){
+            if(count ==0){
+                el = nums[i];
+                count = 1;
+            }
+
+            else if(nums[i]==el){
                 count++;
-            } else {
+            }
+            else if(nums[i]!=el){
                 count--;
             }
-        
-
-            if (count <= 0) {
-                count = 1;
-                majority = i;
-            }
-
         }
 
-      
-        return a[majority];
-    }
+        int ans=0;
 
-    public static boolean checkMajority(int a[], int check) {
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (check == a[i]) {
-                count++;
+        for(int i = 0; i<nums.length;i++){
+            if(nums[i]==el){
+                ans++; 
             }
         }
 
-        if (count > (a.length) / 2) {
-            return true;
-        } else {
-            return false;
+        if(ans>nums.length/2){
+            return el;
         }
-    }
 
-    public static int majorityElement(int a[], int size) {
-
-        int probableElement = majorityProbable(a);
-        
-
-        if (checkMajority(a, probableElement)) {
-            return probableElement;
-        }
         return -1;
+
     }
+
+    
 
     public static void main(String[] args) {
         int N = 5;
         int A[] = { 3, 1, 3, 3, 2 };
-        System.out.println(majorityElement(A, N));
+        System.out.println(majorityElement(A));
     }
 
 }
