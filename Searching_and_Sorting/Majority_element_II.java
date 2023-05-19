788daed4bd1b1ee180ef739d5bc60ac02bdd6f8e
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-19 10:53:27 +0530

    majority_element_II done

---
+++ Searching_and_Sorting/Majority_element_II.java
@@ -0,0 +1,49 @@
+package Searching_and_Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Majority_element_II {
+    public List<Integer> majorityElement(int[] v) {
+        
+        int n = v.length; //size of the array
+
+        int cnt1 = 0, cnt2 = 0; // counts
+        int el1 = Integer.MIN_VALUE; // element 1
+        int el2 = Integer.MIN_VALUE; // element 2
+
+        // applying the Extended Boyer Moore's Voting Algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != v[i]) {
+                cnt1 = 1;
+                el1 = v[i];
+            } else if (cnt2 == 0 && el1 != v[i]) {
+                cnt2 = 1;
+                el2 = v[i];
+            } else if (v[i] == el1) cnt1++;
+            else if (v[i] == el2) cnt2++;
+            else {
+                cnt1--; cnt2--;
+            }
+        }
+
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el1) cnt1++;
+            if (v[i] == el2) cnt2++;
+        }
+
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) ls.add(el1);
+        if (cnt2 >= mini) ls.add(el2);
+
+       
+        //Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
+
+        return ls;
+    }
+}
