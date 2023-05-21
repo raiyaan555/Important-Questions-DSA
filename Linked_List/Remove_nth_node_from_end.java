9a45fa89ec26fb23828f4b7c6b05ae2c5f4a20b9
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-21 15:42:28 +0530

    brute soln removeNthFromEnd1

--- Linked_List/Remove_nth_node_from_end.java
+++ Linked_List/Remove_nth_node_from_end.java
@@ -1,6 +1,37 @@
 package Linked_List;
 
 public class Remove_nth_node_from_end {
+    
+// brute solution
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        if(head.next == null){
+            return null;
+        }
+        
+        ListNode curr = head;
+        int size = 0;
+        
+        while(curr != null){
+            curr = curr.next;
+            size++;
+        }
+        
+        if(n==size){
+            return head.next;
+        }
+        
+        ListNode prev = head;
+        int i = 1;
+        while(i<size-n){
+            prev = prev.next;
+            i++;
+        }
+        
+        prev.next = prev.next.next;
+        return head;
+    }
+
     public class ListNode {
         int val;
         ListNode next;
