9fc40e278c74a2c41883a748185ef1b763619c8b
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-23 09:01:28 +0530

    Copy_List_with_Random_Pointer

---
+++ Linked_List/Copy_List_with_Random_Pointer.java
@@ -0,0 +1,62 @@
+package Linked_List;
+
+
+
+public class Copy_List_with_Random_Pointer {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+    
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    public Node copyRandomList(Node head) {
+        Node iter = head, next;
+
+  // First round: make copy of each node,
+  // and link them together side-by-side in a single list.
+  while (iter != null) {
+    next = iter.next;
+
+    Node copy = new Node(iter.val);
+    iter.next = copy;
+    copy.next = next;
+
+    iter = next;
+  }
+
+  // Second round: assign random pointers for the copy nodes.
+  iter = head;
+  while (iter != null) {
+    if (iter.random != null) {
+      iter.next.random = iter.random.next;
+    }
+    iter = iter.next.next;
+  }
+
+  // Third round: restore the original list, and extract the copy list.
+  iter = head;
+  Node pseudoHead = new Node(0);
+  Node copy, copyIter = pseudoHead;
+
+  while (iter != null) {
+    next = iter.next.next;
+
+    // extract the copy
+    copy = iter.next;
+    copyIter.next = copy;
+    copyIter = copy;
+
+    // restore the original list
+    iter.next = next;
+
+    iter = next;
+  }
+
+  return pseudoHead.next;
+    }
+}
