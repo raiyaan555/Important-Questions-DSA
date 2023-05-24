e4368716e0e5d3dd363439140528ca69016c7a73
Raiyaan Khan <kraiyaan@gmail.com>
2023-05-24 13:09:15 +0530

    trapping rainwater optimal solution

--- Arrays/P11_Trapping_Rain_Water.java
+++ Arrays/P11_Trapping_Rain_Water.java
@@ -1,45 +1,36 @@
 package Arrays;
 public class P11_Trapping_Rain_Water {
-    public static int trap(int[] height) {
-        int[] left = new int[height.length];
-        int leftMax = 0;
-        int rightMax = 0;
-        int[] right = new int[height.length];
-        int sum=0;
-
-        for (int i = 0; i < height.length; i++) {
-            if (leftMax < height[i]) {
-                leftMax = height[i];
+    
+        public static int trap(int[] height) {
+            int l = 0; 
+            int r = height.length-1;
+    
+            int leftMax = 0;
+            int rightMax = 0;
+            int ans = 0;
+    
+            while(l<=r){
+                if(height[l]<=height[r]){
+                    if(height[l]>=leftMax){
+                        leftMax = height[l];
+                    }else{
+                        ans +=leftMax-height[l];
+                    }
+                    l++;
+                }
+    
+                else{
+                    if(height[r]>=rightMax){
+                        rightMax = height[r];
+                    }else{
+                        ans +=rightMax-height[r];
+                    }
+    
+                    r--;
+                }
             }
-             left[i] = leftMax;
-        }
-
-        for (int i = height.length-1; i >= 0; i--) {
-            if (rightMax < height[i]) {
-                rightMax = height[i];
-            }
-            right[i] = rightMax;
-        }
-
-        for (int i = 0; i < height.length; i++) {
-            sum = sum+(Math.min(left[i],right[i]))-height[i];
-        }
-        System.out.println("Left ->");
-        for (int i = 0; i < height.length; i++) {
-            System.out.print(left[i]+" ");
-        }
-        System.out.println();
-        System.out.println("Right ->");
-        for (int i = 0; i < height.length; i++) {
-            System.out.print(right[i]+" ");
-        }
-        System.out.println();
-        System.out.println("Height ->");
-        for (int i = 0; i < height.length; i++) {
-            System.out.print(height[i]+" ");
-        }
-        System.out.println();
-        return sum;
+            return ans;
+        
     }
 
     public static void main(String[] args) {
