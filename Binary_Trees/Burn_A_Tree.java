1219000a50fc81c7f3761e1291fa7e0ba4d563d9
Raiyaan Khan <kraiyaan@gmail.com>
2023-06-09 08:39:28 +0530

    time to burn a tree

---
+++ Binary_Trees/Burn_A_Tree.java
@@ -0,0 +1,84 @@
+package Binary_Trees;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Burn_A_Tree {
+
+   
+    
+    static BinaryTreeNode<Integer> bfsToMapParents(BinaryTreeNode<Integer> root, HashMap<BinaryTreeNode<Integer>, BinaryTreeNode<Integer>> mpp,int start ){
+		Queue<BinaryTreeNode<Integer>> q = new LinkedList<>();
+		q.offer(root);
+		BinaryTreeNode<Integer> res = new BinaryTreeNode<>(-1);
+		while(!q.isEmpty()){
+			BinaryTreeNode<Integer> node = q.poll();
+			if(node.data==start) res=node;
+			if(node.left!=null) {
+				mpp.put(node.left,node);
+				q.offer(node.left);
+			}
+			if(node.right!=null) {
+				mpp.put(node.right,node);
+				q.offer(node.right);
+			}
+		}
+		return res;
+
+	}
+
+	static int findMaxDistance(HashMap<BinaryTreeNode<Integer>, BinaryTreeNode<Integer>> mpp,BinaryTreeNode<Integer> target ){
+		Queue<BinaryTreeNode<Integer>> q = new LinkedList<>();
+		q.offer(target);
+		HashMap<BinaryTreeNode<Integer>,Integer> vis = new HashMap<>();
+		vis.put(target,1);
+		int maxi = 0;
+		while(!q.isEmpty()){
+			int sz = q.size();
+			int fl = 0;
+		
+
+		for(int i = 0; i<sz;i++){
+			BinaryTreeNode<Integer> node = q.poll();
+			if(node.left!=null && vis.get(node.left)==null){
+				fl=1;
+				vis.put(node.left,1);
+				q.offer(node.left);
+			}
+
+			if(node.right!=null && vis.get(node.right)==null){
+				fl=1;
+				vis.put(node.right,1);
+				q.offer(node.right);
+			}
+
+			if(mpp.get(node)!=null && vis.get(mpp.get(node))==null){
+				fl = 1;
+				vis.put(mpp.get(node),1);
+				q.offer(mpp.get(node));
+			}
+		}
+		if(fl==1)maxi++;
+		}
+		return maxi;
+	}
+public static int timeToBurnTree(BinaryTreeNode<Integer> root, int start)
+    {
+        // Write your code here.
+		HashMap<BinaryTreeNode<Integer>, BinaryTreeNode<Integer>> mpp = new HashMap<>();
+		BinaryTreeNode<Integer> target = bfsToMapParents(root,mpp,start);
+		int maxi = findMaxDistance(mpp,target);
+		return maxi;
+    }
+
+}
+class BinaryTreeNode<T> {
+    T data;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
