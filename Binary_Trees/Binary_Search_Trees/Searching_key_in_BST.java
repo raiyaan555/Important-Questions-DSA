13d385189bd29698af6fe154fb6164bcdac37804
Raiyaan Khan <kraiyaan@gmail.com>
2023-06-11 17:42:52 +0530

    Searching_key_in_BST

---
+++ Binary_Trees/Binary_Search_Trees/Searching_key_in_BST.java
@@ -0,0 +1,29 @@
+package Binary_Trees.Binary_Search_Trees;
+
+public class Searching_key_in_BST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
+    }
+}
