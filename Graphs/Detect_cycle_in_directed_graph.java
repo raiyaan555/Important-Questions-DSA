06a15d73844b64fd7c9ba3abe6f666c896d79df6
Raiyaan Khan <kraiyaan@gmail.com>
2023-06-15 05:02:37 +0530

    detect cycle

--- Graphs/Detect_cycle_in_directed_graph.java
+++ Graphs/Detect_cycle_in_directed_graph.java
@@ -6,43 +6,50 @@ import java.util.Queue;
 
 public class Detect_cycle_in_directed_graph {
 
-    boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj){
-
-        int[] indegree = new int[V];
-
-        for(int i=0; i<V;i++){
-            for(int j : adj.get(i)){
-                indegree[j]++;
+     class Pair{
+        int first;
+        int second;
+        
+        Pair(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+    
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        boolean vis[] = new boolean[V];
+        for(int i = 0; i<V;i++){
+            if(vis[i]==false){
+                if(checkForCycle(i,V,adj,vis))return true;
             }
         }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        }
-
-        int topo[] = new int[V];
-
-        int c = 0; 
+        return false;
+    }
+    
+    boolean checkForCycle(int i, int V,  ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+        vis[i] = true;
+        Queue<Pair> q = new LinkedList<>();
+        
+        q.add(new Pair(i, -1));
         while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            c++;
-
+            int node = q.peek().first;
+            int parent = q.peek().second;
+            q.poll();
             
-
-            for(int j: adj.get(node)){
-                indegree[j]--;
-                if(indegree[j]==0){
-                    q.add(j);
+            for(int adjNode : adj.get(node)){
+                if(vis[adjNode]==false){
+                    vis[adjNode] = true;
+                    q.add(new Pair(adjNode, node));
+                }
+                // if parent is not adj node then only visited because there is a cycle
+                else if (parent!=adjNode){
+                    return true;
                 }
             }
         }
-        if(c!=V) return false;
-
-        return true;
+        return false;
     }
 }
