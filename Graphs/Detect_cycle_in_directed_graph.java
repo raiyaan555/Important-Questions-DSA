e224ab6066f61f53ff4be4b3c1d0ed70e8bac662
Raiyaan Khan <kraiyaan@gmail.com>
2023-06-16 15:01:53 +0530

    detect a cycle in directed graph

--- Graphs/Detect_cycle_in_directed_graph.java
+++ Graphs/Detect_cycle_in_directed_graph.java
@@ -52,4 +52,44 @@ public class Detect_cycle_in_directed_graph {
         }
         return false;
     }
+
+
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        
+        int vis[] = new int[V];
+        int pathVis[] = new int[V];
+        
+        for(int i = 0; i<V;i++){
+            if(vis[i]==0){
+                if(dfs(i,adj,vis,pathVis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+    
+    boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int[] pathVis)
+    {
+    vis[node] = 1;
+    pathVis[node] = 1;
+    
+    
+    for(int i: adj.get(node)){
+        if(vis[i]==0)
+        {
+            if(dfs(i,adj,vis,pathVis)){
+                return true;
+            }
+        }else if(pathVis[i]==1){
+            return true;
+        }
+    }
+    
+    pathVis[node]=0;
+    return false;
+    
+}
 }
