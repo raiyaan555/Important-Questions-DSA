60c6eaab046f96fa788d30e743d5518df802bdb4
Raiyaan Khan <kraiyaan@gmail.com>
2023-06-20 22:47:47 +0530

    competitive code

---
+++ Competitive_Coding/BitPlusPlus.java
@@ -0,0 +1,30 @@
+package Competitive_Coding;
+
+
+import java.util.Scanner;
+
+public class BitPlusPlus {
+   static int sum = 0;
+
+    public static int bitcall(String s){
+        
+
+        
+        String a = "++X";
+        String b = "X++";
+
+        if(s.equalsIgnoreCase(a)|| s.equalsIgnoreCase(b)){
+            return ++sum;
+        }else return --sum;
+    }
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+     int n = sc.nextInt();
+
+     while(n-->0){
+     String s = sc.next();
+     System.out.println(bitcall(s));
+     }
+    }
+}
