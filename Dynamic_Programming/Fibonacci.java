482a0e292bcc6203a863a643844138b98c8fa870
Raiyaan Khan <kraiyaan@gmail.com>
2023-07-01 11:48:40 +0530

    fibonacci

--- Dynamic_Programming/Fibonacci.java
+++ Dynamic_Programming/Fibonacci.java
@@ -2,6 +2,8 @@ package Dynamic_Programming;
 
 import java.util.Arrays;
 
+// Memoization
+ 
 public class Fibonacci {
 
     public static int fib(int n, int[] dp) {
@@ -21,6 +23,41 @@ public class Fibonacci {
         int dp[] = new int[n + 1];
         Arrays.fill(dp, -1);
         System.out.println(fib(n, dp));
+
+    }
+
+    // Tabulation
+    
+     public static int fibTab(int n, int[] dp) {
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+
+
+    // Space optimization
+
+
+    public static int fib3(int n) {
+
+        int prev2 = 0;
+        int prev= 1;
+        int curr = -1;
+
+        for(int i = 2; i<=n;i++){
+          curr = prev+prev2;
+          prev2 = prev;
+          prev = curr;
+        }
+
+        return prev;
 
     }
 }
