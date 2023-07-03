9eeaaa78d6d124836ea423a1a0aa38de71a79f9d
Raiyaan Khan <kraiyaan@gmail.com>
2023-07-03 09:01:00 +0530

    ninja training

---
+++ Dynamic_Programming/Ninja_Training.java
@@ -0,0 +1,121 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class Ninja_Training {
+
+
+    // Memoization
+
+
+    static int f(int day, int last, int[][] points, int[][] dp) {
+
+        if (dp[day][last] != -1)
+            return dp[day][last];
+
+        if (day == 0) {
+            int maxi = 0;
+            for (int i = 0; i <= 2; i++) {
+                if (i != last)
+                    maxi = Math.max(maxi, points[0][i]);
+            }
+            return dp[day][last] = maxi;
+        }
+
+        int maxi = 0;
+        for (int i = 0; i <= 2; i++) {
+            if (i != last) {
+                int activity = points[day][i] + f(day - 1, i, points, dp);
+                maxi = Math.max(maxi, activity);
+            }
+
+        }
+
+        return dp[day][last] = maxi;
+    }
+
+    static int ninjaTraining(int n, int[][] points) {
+
+        int dp[][] = new int[n][4];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return f(n - 1, 3, points, dp);
+    }
+
+
+    // Tabulation
+
+   
+    static int ninjaTraining2(int n, int[][] points) {
+
+
+        int[][] dp = new int[n][4];
+        dp[0][0] = Math.max(points[0][1], points[0][2]);
+        dp[0][1] = Math.max(points[0][0], points[0][2]);
+        dp[0][2] = Math.max(points[0][0], points[0][1]);
+        dp[0][3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
+
+        for (int day = 1; day < n; day++) {
+            for (int last = 0; last < 4; last++) {
+                dp[day][last] = 0;
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) {
+                        int activity = points[day][task] + dp[day - 1][task];
+                        dp[day][last] = Math.max(dp[day][last], activity);
+                    }
+                }
+            }
+
+        }
+
+        return dp[n - 1][3];
+    }
+
+
+    // Optimal
+
+    static int ninjaTraining3(int n, int[][] points) {
+
+
+        int prev[] = new int[4];
+
+        prev[0] = Math.max(points[0][1], points[0][2]);
+        prev[1] = Math.max(points[0][0], points[0][2]);
+        prev[2] = Math.max(points[0][0], points[0][1]);
+        prev[3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
+
+        for (int day = 1; day < n; day++) {
+
+            int temp[] = new int[4];
+            for (int last = 0; last < 4; last++) {
+                temp[last] = 0;
+                for (int task = 0; task <= 2; task++) {
+                    if (task != last) {
+                        temp[last] = Math.max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+
+            prev = temp;
+
+        }
+
+        return prev[3];
+    }
+
+    public static void main(String args[]) {
+
+        int[][] points = { { 10, 40, 70 },
+                { 20, 50, 80 },
+                { 30, 60, 90 } };
+
+        int n = points.length;
+        System.out.println(ninjaTraining(n, points));
+    }
+
+
+    
+}
+
+    
