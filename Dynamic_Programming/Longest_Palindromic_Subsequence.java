f1fa15904e528d2ba7df05dfda5bbf49744f096e
Raiyaan Khan <kraiyaan@gmail.com>
2023-07-12 13:35:59 +0530

    Longest_Palindromic_Subsequence

---
+++ Dynamic_Programming/Longest_Palindromic_Subsequence.java
@@ -0,0 +1,66 @@
+package Dynamic_Programming;
+
+public class Longest_Palindromic_Subsequence {
+    class Solution {
+        String lcs(String s1, String s2) {
+    
+        int n=s1.length();
+        int m=s2.length();
+    
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 0;
+        }
+    
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1.charAt(ind1-1)==s2.charAt(ind2-1))
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else
+                    dp[ind1][ind2] = 0 + Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            }
+        }
+    
+        int len=dp[n][m];
+        int i=n;
+        int j=m;
+    
+        int index = len-1;
+        String str="";
+        for(int k=1; k<=len;k++){
+            str +="$"; // dummy string
+        }
+        StringBuilder ss= new StringBuilder(s1);
+        StringBuilder str2=new StringBuilder(str);
+        while(i>0 && j>0){
+            if(ss.charAt(i-1) == s2.charAt(j-1)){
+                str2.setCharAt(index,ss.charAt(i-1) );
+                index--;
+                i--;
+                j--;
+            }
+            else if(ss.charAt(i-1)>s2.charAt(j-1)){
+                i--;
+            }
+            else j--;
+        }
+        return(str2).toString();
+    }
+        public int longestPalindromeSubseq(String s) {
+            StringBuilder s1 = new StringBuilder();
+     
+            
+            s1.append(s);
+     
+            
+            String s2 = s1.reverse().toString();
+    
+            return lcs(s,s2).length();
+    
+     
+        }
+    }
+}
