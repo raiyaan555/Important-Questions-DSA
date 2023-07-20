035b1ea30603b5fd2a7b8c6f517f48ebf404d772
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-07-20 22:28:32 +0530

    commit

--- Dynamic_Programming/Distinct_Subsequences.java
+++ Dynamic_Programming/Distinct_Subsequences.java
@@ -9,10 +9,11 @@ public class Distinct_Subsequences {
     
           
                 dp[0] = 1;
-                
+
             
     
             
+                
             for(int i = 1;i<=s.length();i++){
                 for(int j=t.length();j>=1;j--){
                     if(s.charAt(i-1)==t.charAt(j-1)){
