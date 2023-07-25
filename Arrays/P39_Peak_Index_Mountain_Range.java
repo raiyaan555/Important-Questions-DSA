3b11257e3898428fd16586c7069d744c9716ba63
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-07-25 23:13:48 +0530

    peak index

---
+++ Arrays/P39_Peak_Index_Mountain_Range.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class P39_Peak_Index_Mountain_Range {
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int lo = 0;
+            int hi = arr.length - 1;
+            int mid;
+            while (lo < hi) {
+                mid = (hi + lo) / 2;
+                if (arr[mid] < arr[mid + 1]) {
+                    lo = mid + 1;
+                    ;
+                } else {
+                    hi = mid;
+                }
+            }
+            return lo;
+        }
+    }
+}
