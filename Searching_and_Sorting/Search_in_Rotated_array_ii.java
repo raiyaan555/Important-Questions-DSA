7757ddcd55a118893c7e11677c68a2249860243a
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-08-10 08:56:23 +0530

    better approach search in rotated array

--- Searching_and_Sorting/Search_in_Rotated_array_ii.java
+++ Searching_and_Sorting/Search_in_Rotated_array_ii.java
@@ -2,6 +2,53 @@ package Searching_and_Sorting;
 
 public class Search_in_Rotated_array_ii {
     class Solution {
+
+        // better approach
+
+        public boolean search2(int[] nums, int target) {
+            // if(nums.length==1){
+            // if (target==nums[0]) return true; else return false;
+            // }
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    return true;
+                }
+
+                // lets assume the right side is sorted and left side is not sorted
+                if (nums[mid] < nums[left] || nums[mid] < nums[right]) {
+                    if (target > nums[mid] && target <= nums[right]) {
+                        left = mid + 1;
+                    } else
+                        right = mid - 1;
+                }
+
+                // lets assume the left side is sorted and the right side is not sorted
+
+                else if (nums[mid] > nums[left] || nums[mid] > nums[right]) {
+                    if (target < nums[mid] && target >= nums[left]) {
+                        right = mid - 1;
+                    } else
+                        left = mid + 1;
+                }
+                // If we get here, that means nums[left] == nums[mid] == nums[right], then
+                // shifting out
+                // any of the two sides won't change the result but can help remove duplicate
+                // from
+                // consideration, here we just use right-- but left++ works too
+                else {
+                    right--;
+                }
+            }
+            return false;
+        }
+        
+
         public boolean binarySearch(int lowerLimit, int upperLimit, int[] arr, int target) {
             while (lowerLimit <= upperLimit) {
                 int mid = lowerLimit + (upperLimit - lowerLimit) / 2;
