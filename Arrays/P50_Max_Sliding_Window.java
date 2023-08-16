2ba605e555a4d640180d12123bae92bec1ecaacc
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-08-16 19:50:13 +0530

    Max Sliding window

---
+++ Arrays/P50_Max_Sliding_Window.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class P50_Max_Sliding_Window {
+    class Solution {
+    public int[] maxSlidingWindow(int[] a, int k) {		
+		if (a == null || k <= 0) {
+			return new int[0];
+		}
+		int n = a.length;
+		int[] r = new int[n-k+1];
+		int ri = 0;
+		// store index
+		Deque<Integer> q = new ArrayDeque<>();
+		for (int i = 0; i < a.length; i++) {
+			// remove numbers out of range k
+			while (!q.isEmpty() && q.peek() < i - k + 1) {
+				q.poll();
+			}
+			// remove smaller numbers in k range as they are useless
+			while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+				q.pollLast();
+			}
+			// q contains index... r contains content
+			q.offer(i);
+			if (i >= k - 1) {
+				r[ri++] = a[q.peek()];
+			}
+		}
+		return r;
+	}
+}
+}
