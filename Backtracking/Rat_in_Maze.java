69bb48d5703a42c3697072ab558dfa82fa81726d
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-08-23 21:30:57 +0530

    Rat_In_Maze.java

--- Backtracking/Rat_in_Maze.java
+++ Backtracking/Rat_in_Maze.java
@@ -1,85 +1,86 @@
 package Backtracking;
 
-public class Rat_in_Maze {
-  
-    static int N;
-  
-    
-    void printSolution(int sol[][])
-    {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(
-                    " " + sol[i][j] + " ");
-            System.out.println();
+import java.util.*;
+
+class Rat_in_Maze {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] a = new int[n][n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    a[i][j] = sc.nextInt();
+
+            Solution obj = new Solution();
+            ArrayList<String> res = obj.findPath(a, n);
+            Collections.sort(res);
+            if (res.size() > 0) {
+                for (int i = 0; i < res.size(); i++)
+                    System.out.print(res.get(i) + " ");
+                System.out.println();
+            } else {
+                System.out.println(-1);
+            }
         }
     }
-  
-    
-    boolean isSafe(
-        int maze[][], int x, int y)
-    {
-        
-        return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1);
-    }
-  
- 
-    boolean solveMaze(int maze[][])
-    {
-        int sol[][] = new int[N][N];
-  
-        if (solveMazeUtil(maze, 0, 0, sol) == false) {
-            System.out.print("Solution doesn't exist");
-            return false;
+}
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+// m is the given matrix and n is the order of matrix
+class Solution {
+    private static void solve(int i, int j, int a[][], int n, ArrayList<String> ans, String move,
+            int vis[][]) {
+        if (i == n - 1 && j == n - 1) {
+            ans.add(move);
+            return;
         }
-  
-        printSolution(sol);
-        return true;
-    }
-  
-   
-    boolean solveMazeUtil(int maze[][], int x, int y, int sol[][])
-    {
-        
-        if (x == N - 1 && y == N - 1 && maze[x][y] == 1) {
-            sol[x][y] = 1;
-            return true;
+
+        // downward
+        if (i + 1 < n && vis[i + 1][j] == 0 && a[i + 1][j] == 1) {
+            vis[i][j] = 1;
+            solve(i + 1, j, a, n, ans, move + 'D', vis);
+            vis[i][j] = 0;
         }
-  
-        
-        if (isSafe(maze, x, y)) {
-            
-              if (sol[x][y] == 1)
-                  return false;
-            
-            
-            sol[x][y] = 1;
-  
-          
-            if (solveMazeUtil(maze, x + 1, y, sol))
-                return true;
-  
-            
-            if (solveMazeUtil(maze, x, y + 1, sol))
-                return true;
-  
-           
-            sol[x][y] = 0;
-            return false;
+
+        // left
+        if (j - 1 >= 0 && vis[i][j - 1] == 0 && a[i][j - 1] == 1) {
+            vis[i][j] = 1;
+            solve(i, j - 1, a, n, ans, move + 'L', vis);
+            vis[i][j] = 0;
         }
-  
-        return false;
+
+        // right
+        if (j + 1 < n && vis[i][j + 1] == 0 && a[i][j + 1] == 1) {
+            vis[i][j] = 1;
+            solve(i, j + 1, a, n, ans, move + 'R', vis);
+            vis[i][j] = 0;
+        }
+
+        // upward
+        if (i - 1 >= 0 && vis[i - 1][j] == 0 && a[i - 1][j] == 1) {
+            vis[i][j] = 1;
+            solve(i - 1, j, a, n, ans, move + 'U', vis);
+            vis[i][j] = 0;
+        }
     }
-  
-    public static void main(String args[])
-    {
-        Rat_in_Maze rat = new Rat_in_Maze();
-        int maze[][] = { { 1, 0, 0, 0 },
-                         { 1, 1, 1, 1 },
-                         { 0, 0, 1, 1 },
-                         { 1, 1, 1, 1 } };
-  
-        N = maze.length;
-        rat.solveMaze(maze);
+
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        int vis[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                vis[i][j] = 0;
+            }
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+        if (m[0][0] == 1)
+            solve(0, 0, m, n, ans, "", vis);
+        return ans;
     }
 }
