6e051603ca4bb2ff494fdddea9a2f22da8b5f06d
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-08-26 22:43:09 +0530

    Max_Length_of_Pair_Chain

---
+++ Greedy/Max_Length_of_Pair_Chain.java
@@ -0,0 +1,22 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class Max_Length_of_Pair_Chain {
+    public class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        int cur = Integer.MIN_VALUE, ans = 0;
+        
+        for (int[] pair : pairs) {
+            if (cur < pair[0]) {
+                cur = pair[1];
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
+}
