2bb2f312960af76153006e236086e9359e2d2f9a
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-09-01 22:45:55 +0530

    counting_bits.java

---
+++ Bit_Manipulation/Counting_Bits.java
@@ -0,0 +1,20 @@
+package Bit_Manipulation;
+
+public class Counting_Bits {
+    class Solution {
+        public int[] countBits(int n) {
+            int [] ans = new int[n+1];
+    
+            for(int i = 0; i<=n;i++){
+                int a = i;
+                int cnt = 0;
+                while(a!=0){
+                    a = a&a-1;
+                    cnt++;
+                }
+                ans[i] = cnt;
+            }
+            return ans;
+        }
+    }
+}
