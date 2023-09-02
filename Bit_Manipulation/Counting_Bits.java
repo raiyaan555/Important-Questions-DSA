9a76332c7c7c7966be0ed2369d0fa528d1659b21
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-09-02 21:29:41 +0530

    bitwise manipulation

--- Bit_Manipulation/Counting_Bits.java
+++ Bit_Manipulation/Counting_Bits.java
@@ -3,6 +3,7 @@ package Bit_Manipulation;
 public class Counting_Bits {
     class Solution {
         public int[] countBits(int n) {
+            
             int [] ans = new int[n+1];
     
             for(int i = 0; i<=n;i++){
