bda113298e9be66022b8084537227165987a2385
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-10-04 18:01:55 +0530

    design hashmap

---
+++ Heap_and_Hashing/Design_HashMap.java
@@ -0,0 +1,22 @@
+package Heap_and_Hashing;
+
+import java.util.Arrays;
+
+public class Design_HashMap {
+    class MyHashMap {
+    int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
+}
