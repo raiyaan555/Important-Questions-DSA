640fb9a915434cd5d0191b0b273ecac78306c42f
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-10-14 21:08:01 +0530

    Painting_the_walls

---
+++ Dynamic_Programming/Painting_the_Walls.java
@@ -0,0 +1,19 @@
+package Dynamic_Programming;
+import java.util.Arrays;
+
+public class Painting_the_Walls {
+  class Solution {
+    int[][] memo;
+    int n;
+    
+        public int paintWalls(int[] cost, int[] time) {
+        int n = cost.length, dp[] = new int[n + 1];
+        Arrays.fill(dp, (int)1e9);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = n; j > 0; --j)
+                dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
+        return dp[n];
+    }
+}  
+}
