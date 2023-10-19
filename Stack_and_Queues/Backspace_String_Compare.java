b1f808b3b1c1e89bd3af6f3c3b5f63915c1956de
Raiyaan Khan <74910047+raiyaan555@users.noreply.github.com>
2023-10-19 19:54:35 +0530

    Backspace_Question

---
+++ Stack_and_Queues/Backspace_String_Compare.java
@@ -0,0 +1,31 @@
+package Stack_and_Queues;
+
+public class Backspace_String_Compare {
+    class Solution {
+        public boolean backspaceCompare(String s, String t) {
+            Stack<Character> st1 = new Stack<>();
+            Stack<Character> st2 = new Stack<>();
+    
+            for(char c: s.toCharArray()){
+                if(c!='#'){
+                    st1.push(c);
+                }else{
+                    if(!st1.isEmpty())
+                    st1.pop();
+                }
+            }
+    
+            for(char c: t.toCharArray()){
+                if(c!='#'){
+                    st2.push(c);
+                }else{
+                    if(!st2.isEmpty())
+                    st2.pop();
+                }
+            }
+    
+            if(st1.equals(st2)) return true;
+            return false;
+        }
+    }
+}
